c444429e9685518a591005def01eda85946451ea
Roberto C Navarro <rnavarro70@gmail.com>
2018-03-29 14:36:20 -0500

    first commit

---
+++ Delegates/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var photoProcessor = new PhotoProcessor();
+            photoProcessor.Process();
+        }
+    }
+
+
+    public class PhotoProcessor
+    {
+
+        public void Process()
+        {
+            var photo = new Photo();
+
+            var filters = new PhotoFilters();
+            filters.ApplyBrightness(photo);
+            filters.ApplyContrast(photo);
+            filters.Resize(photo);
+
+            photo.Save();
+        }
+    }
+
+    public class PhotoFilters
+    {
+        public void ApplyBrightness(Photo photo)
+        {
+            Console.WriteLine("Apply brightness");
+        }
+
+        public void ApplyContrast(Photo photo)
+        {
+            Console.WriteLine("Apply contrast");
+        }
+
+        public void Resize(Photo photo)
+        {
+            Console.WriteLine("Resize photo");
+        }
+
+    }
+
+    public class Photo
+    {
+        public void Save()
+        {
+            // Saving logic...
+        }
+    }
+}
